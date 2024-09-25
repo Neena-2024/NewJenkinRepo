544fd4d8461e54a878d89475df244ce7d1240ca8
neena <neena@Neena.lan>
2024-09-25 19:25:40 +0530

    My first commit

---
+++ NewJenkinPgm/src/main/java/pgms/Add.java
@@ -0,0 +1,13 @@
+package pgms;
+
+public class Add {
+	
+
+	public static void main(String[] args) {
+		int a=2,b=3,sum=0;
+		sum=a+b;
+		System.out.println("Sum is : "+sum);
+
+	}
+
+}
---
+++ NewJenkinPgm/src/main/java/pgms/Method1.java
@@ -0,0 +1,14 @@
+package pgms;
+
+public class Method1 {
+	public static void show()
+	{
+		System.out.println("Hello..");
+	}
+
+	public static void main(String[] args) {
+		Method1.show();
+
+	}
+
+}
